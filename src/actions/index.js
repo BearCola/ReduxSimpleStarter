d90e5cdf8b59b765269f525a9bae21f00d5d2580
Sangin <sangin.kang@navercorp.com>
2019-01-24 07:28:17 +0900

    Update searchbar

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '9a1885cd257de068f957c6ea2fb6eb44';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FECTH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER
+    };
+}
