e57e7d4ba4ce0062a8844537b026c6a75c8c771f
Sangin <sangin.kang@navercorp.com>
2019-01-25 08:05:42 +0900

    Update code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
 const API_KEY = '9a1885cd257de068f957c6ea2fb6eb44';
-// const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?lat=48.13&lon=17.12&APPID=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FECTH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-
-    console.log('Request:', request);
 
     return {
         type: FETCH_WEATHER,
