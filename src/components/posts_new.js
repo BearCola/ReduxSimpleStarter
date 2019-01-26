371e7aafbb0d596f76a8c255256c30b8fbceb0ea
Sangin <sangin.kang@navercorp.com>
2019-01-27 08:49:21 +0900

    Update code

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
     render() {
@@ -7,21 +8,30 @@ class PostsNew extends Component {
         console.log(title);
 
         return (
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea type="text" className="form-control" {...content}/>
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
@@ -30,7 +40,27 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a username';
+    }
+
+    if (!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+
+    if (!values.content) {
+        errors.content = 'Enter some contents';
+    }
+
+
+    return errors;
+}
+
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']
-})(PostsNew);
+    fields: ['title', 'categories', 'content'],
+    validate
+}, null, { createPost})(PostsNew);
